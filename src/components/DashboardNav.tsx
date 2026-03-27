d78e437540dab48a7642ae7c3fc7030b3ead7d8d
Sanjay Kr Singh <uideveloper09@gmail.com>
2026-03-27 14:58:20 +0000

    Add LSF logo asset

--- src/components/DashboardNav.tsx
+++ src/components/DashboardNav.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import lsfLogo from "@/assets/lsf-logo.png";
 
 const navItems = ["DASHBOARD", "PLANS", "COMPARISON", "SUPPORT", "ACCOUNT"];
 
@@ -8,17 +9,8 @@ const DashboardNav = () => {
   return (
     <nav className="flex items-center px-8 py-3 border-b" style={{ backgroundColor: 'hsl(215 35% 15%)', borderBottomColor: 'hsl(215 25% 25%)' }}>
       {/* Logo */}
-      <div className="relative mr-12 flex items-center">
-        <svg width="80" height="36" viewBox="0 0 80 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-          {/* L */}
-          <text x="0" y="26" fontFamily="Orbitron, sans-serif" fontSize="28" fontWeight="800" fontStyle="italic" fill="white">L</text>
-          {/* S - orange */}
-          <text x="18" y="26" fontFamily="Orbitron, sans-serif" fontSize="28" fontWeight="800" fontStyle="italic" fill="#e8792b">S</text>
-          {/* F */}
-          <text x="38" y="26" fontFamily="Orbitron, sans-serif" fontSize="28" fontWeight="800" fontStyle="italic" fill="white">F</text>
-          {/* Orange swoosh underline */}
-          <path d="M2 32 Q20 28 40 30 Q55 32 75 29" stroke="#e8792b" strokeWidth="2.5" strokeLinecap="round" fill="none"/>
-        </svg>
+      <div className="mr-12 flex items-center">
+        <img src={lsfLogo} alt="LSF" className="h-8" />
       </div>
 
       {/* Nav Items - spread evenly */}
