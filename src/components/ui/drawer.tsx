e8b065d5ca2d8bb6f43d19c61af17059ab5d2a7f
Sanjay Kr Singh <uideveloper09@gmail.com>
2026-03-29 20:11:59 +0000

    Style drawer handle matching navy

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -36,7 +36,7 @@ const DrawerContent = React.forwardRef<
       )}
       {...props}
     >
-      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />
+      <div className="mx-auto mt-4 h-1.5 w-[60px] rounded-full" style={{ backgroundColor: 'hsl(215 30% 35%)' }} />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
